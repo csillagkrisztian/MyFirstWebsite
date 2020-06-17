07408e2c5b86007060bc2e8cefa963232792af40
Csillag Krisztián <krisztian.csillag93@gmail.com>
2020-06-17 09:58:08 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myImage1 = document.querySelector('.img1');
+let myImage2 = document.querySelector('.img2');
+let myImage3 = document.querySelector('.img3');
+let myImage4 = document.querySelector('.img4');
+let myImage5 = document.querySelector('.img5');
+let myImage6 = document.querySelector('.img6');
+
+
+
+myImage.onclick = function(){
+        alert("Hey! Please don't poke my creator!");
+    }
+myImage1.onclick = function(){
+        alert("This was my first attempt at drawing pixel icons!");
+    }
+myImage2.onclick = function(){
+        alert("With the help of a Udemy class, I made my first publicly published browser game in 2019!");
+    }
+myImage3.onclick = function(){
+        alert("My first ever serious background, where I was inspired by the PS4 game Bloodborne!");
+    }
+myImage4.onclick = function(){
+        alert("I bake my own croissants! Yes, it does take forever. Yes, it is better than anything you can buy in a store.");
+    }
+myImage5.onclick = function(){
+        alert("My daily intake of Vitamin-PS4 sometimes can jump to unhealthy doses. I have been gaming since I was 5 and haven't stopped since.");
+    }
+myImage6.onclick = function(){
+        alert("That is me singing in front of 2000 people in the center of Subotica. You are guessing right, I was nearly wetting myself due to excitement!");
+    }
+
+
+myButton.onclick = function(){
+    location.href="https://kikiop1313.itch.io/lazer-defender-returnofudemy";
+}
+
+
+
+
+
